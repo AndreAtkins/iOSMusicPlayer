2db28043c52173d10825de38c0f169882e502af5
AndreAtkins <aatkins00@student.coppin.edu>
2020-06-22 20:03:49 -0400

    Initial Commit

---
+++ CrankDaGang/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CrankDaGang
+//
+//  Created by Andre Atkins on 6/22/20.
+//  Copyright © 2020 Andre Atkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
