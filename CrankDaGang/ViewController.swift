a08af566758bbf3e2e6bde5a6b988905ecb51571
AndreAtkins <aatkins00@student.coppin.edu>
2020-06-23 17:21:37 -0400

    "original push to remote"

--- CrankDaGang/ViewController.swift
+++ CrankDaGang/ViewController.swift
@@ -5,16 +5,53 @@
 //  Created by Andre Atkins on 6/22/20.
 //  Copyright © 2020 Andre Atkins. All rights reserved.
 //
-
+import MediaPlayer
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+   var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
+       
+       override func viewDidLoad() {
+           super.viewDidLoad()
 
+     
+       }
+       
+       @IBAction func stopButtonTapped(_ sender: UIButton) {
+           musicPlayer.stop()
+       }
+       @IBAction func nextButtonTapped(_ sender: UIButton) {
+           musicPlayer.skipToNextItem()
+       }
+       @IBAction func previousButtonTapped(_ sender: UIButton) {
+           musicPlayer.skipToPreviousItem()
+       }
+       @IBAction func musicButtonTapped(_ sender: UIButton) {
+           
+           MPMediaLibrary.requestAuthorization {(status) in
+               if status == .authorized{
+                  DispatchQueue.main.async {
+                   self.playArtist(artist: sender.currentTitle!)
+                   }
+                }
+           }
+           
+           playArtist(artist: sender.currentTitle!)
+       }
+       
+       func playArtist (artist: String){
+           
+           musicPlayer.stop();
+           
+           let query = MPMediaQuery()
+           let predicate = MPMediaPropertyPredicate(value: artist, forProperty: MPMediaItemPropertyArtist)
+           query.addFilterPredicate(predicate)
+           musicPlayer.setQueue(with: query)
+           musicPlayer.shuffleMode = .songs
+           musicPlayer.play()
+       }
+       
 
 }
 
